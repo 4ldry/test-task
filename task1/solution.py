ad01e288acad86445546395a92a746f24c6e3eee
4ldry <saimonkstudio@gmail.com>
2025-02-16 14:05:14 +0300

    Used pathlib instead of os module

--- task1/solution.py
+++ task1/solution.py
@@ -1,20 +1,24 @@
 import json
-import os
 import matplotlib.pyplot as mpl
 from collections import defaultdict
+from pathlib import Path
 
-dataset_path = os.path.dirname(os.path.realpath(__file__)) + r"\dataset\filtered_traces"
-apps = os.listdir(dataset_path)
-apps_count = len(apps)
+task_path = Path(__file__).parent
+dataset_path = Path("dataset/filtered_traces")
+gestures = Path("gestures.json")
 traces_by_len: defaultdict[int, int] = defaultdict(int)
 
 
+apps_path_generator = Path(task_path / dataset_path).iterdir()
+apps_count = 0
+
 def trace_generator():
-    for app_path in apps:
-        app_directory = dataset_path + rf"\{app_path}"
-        traces = os.listdir(app_directory)
-        for trace_path in traces:
-            gestures_path = app_directory + rf"\{trace_path}" + r"\gestures.json"
+    global apps_count
+    for app_path in apps_path_generator:
+        apps_count += 1
+        traces_path_generator = app_path.iterdir()
+        for trace_path in traces_path_generator:
+            gestures_path = trace_path / gestures
             with open(gestures_path, "r") as file:
                 gestures_content = json.load(file)
             trace_length = len(gestures_content)
@@ -34,13 +38,14 @@ def main():
     print(f"Количество приложений - {apps_count}")
     print(f"Количество трейсов - {traces_count}")
     print(
-        f"Самый длинный трейс ({max_trace_length}) - {max_trace_num} в приложении {max_trace_app}"
+        f"Самый длинный трейс ({max_trace_length}) - {max_trace_num.stem} в приложении {max_trace_app.stem}"
     )
+    print(traces_by_len)
     mpl.bar(list(traces_by_len.keys()), list(traces_by_len.values()), color="g")
     mpl.title("Распределение трейсов по длине")
     mpl.xlabel("Длина трейсов")
     mpl.ylabel("Кол-во трейсов")
-    mpl.savefig("task1/solution1")
+    mpl.savefig(f"{task_path}/solution1")
 
 if __name__ == "__main__":
     main()
