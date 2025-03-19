93cf3f8ac560a2c8a637fd86fb505c8858d0db59
4ldry <saimonkstudio@gmail.com>
2025-03-19 22:18:35 +0300

    Finalized solution for task 3

--- task3/solution.py
+++ task3/solution.py
@@ -1,7 +1,8 @@
 from transformers import T5ForConditionalGeneration, AutoTokenizer
 from evaluate import load
 from dataclasses import dataclass
-import json
+from datasets import load_dataset, Dataset
+import argparse
 
 
 @dataclass
@@ -12,8 +13,19 @@ class Solution:
     tokenizer = AutoTokenizer.from_pretrained(checkpoint)
     model = T5ForConditionalGeneration.from_pretrained(checkpoint).to(device)
 
-    def get_preds_refs(self, record: dict) -> tuple[str, str]:
-        src_code = record["result_masked_no_coms"]
+    def load_dataset(self, filepath: str, shuffle: bool) -> Dataset:
+        dataset = load_dataset("json", data_files=filepath)
+        sample_size = 1000
+        if shuffle:
+            dataset = dataset.shuffle(seed=42)
+        sampled_dataset = dataset["train"].select(range(sample_size))
+        return sampled_dataset
+
+    def get_preds_refs(self, record: dict, mode: str) -> tuple[str, str]:
+        if mode == "with_coms":
+            src_code = record["result_masked_with_coms"]
+        elif mode == "no_coms":
+            src_code = record["result_masked_no_coms"]
         encoded = self.tokenizer.encode_plus(
             src_code,
             add_special_tokens=True,
@@ -37,8 +49,8 @@ class Solution:
             references=references, predictions=predictions
         )["rouge1"]
         print(f"\
-        Exact match is {round(em_result, 10)} \n\
-        Rouge-1 is {round(rouge_result, 10)}")
+        Exact match is {round(em_result, 4)} \n\
+        Rouge-1 is {round(rouge_result, 4)}")
 
     def filter_tokens(self, output, end_token="<extra_id_1>") -> str:
         txt = self.tokenizer.decode(
@@ -51,16 +63,38 @@ class Solution:
             return txt.lstrip(" ")
 
 
+def arg_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Solution for task 3")
+    parser.add_argument(
+        "-dataset_mode",
+        required=True,
+        choices=["with_coms", "no_coms"],
+        type=str,
+        help="Choice between funcs' data with coms and w/o coms",
+    )
+    parser.add_argument(
+        "-dataset_filepath",
+        required=False,
+        default="result.jsonl",
+        type=str,
+        help="Path to dataset file from current dir",
+    )
+    args = parser.parse_args()
+    return args
+
+
 def main():
+    cli_args = arg_parser()
+    mode = cli_args.dataset_mode
+    filepath = cli_args.dataset_filepath
     solution = Solution()
     predictions = []
     references = []
-    with open("result.jsonl", "r", encoding="utf-8") as data:
-        for line in data:
-            record = json.loads(line)
-            pred, ref = solution.get_preds_refs(record)
-            predictions.append(pred)
-            references.append(ref)
+    dataset = solution.load_dataset(filepath=filepath, shuffle=False)
+    for record in dataset:
+        pred, ref = solution.get_preds_refs(record, mode)
+        predictions.append(pred)
+        references.append(ref)
     solution.evaluate_model(predictions, references)
 
 
